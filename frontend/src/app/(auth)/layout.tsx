4f9e7acb2ade1e8d79531800fcb2eb79cdffdc32
Siddharth Kundu <sk31395@gmail.com>
2025-08-13 15:19:00 -0400

    imported Toaster from shadcn/ui

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Providers } from "~/components/providers";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -21,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
