27e4f532a5ed54410753ad3dc4f9c64f14f7d035
Siddharth Kundu <sk31395@gmail.com>
2025-08-13 18:56:51 -0400

    initial test to check the concurrency of inngest and the API calls to Modal

---
+++ frontend/src/actions/generation.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { inngest } from "~/inngest/client";
+import { auth } from "~/lib/auth";
+import { db } from "~/server/db";
+
+export async function queueSong() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+  const song = await db.song.create({
+    data: {
+      userId: session?.user.id,
+      title: "TestSong 1",
+      fullDescribedSong: "Hio-hop song",
+    },
+  });
+
+  await inngest.send({
+    name: "generate-song-event",
+    data: {
+      songId: song.id,
+      userId: song.userId,
+    },
+  });
+}
