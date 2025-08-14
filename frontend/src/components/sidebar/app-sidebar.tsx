ebbcc46b0b6c904f148f46314c11862bf06498dc
Siddharth Kundu <sk31395@gmail.com>
2025-08-14 16:51:15 -0400

    implementation of statte management using zustand. sidebar creation and relevant components

---
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,50 @@
+"use server";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+} from "~/components/ui/sidebar";
+import SidebarMenuItems from "./sidebar-menu-items";
+import { Credits } from "./credits";
+import { UserButton } from "@daveyplate/better-auth-ui";
+import { User } from "lucide-react";
+
+export async function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-primary mt-4 mb-12 flex flex-col items-start justify-start px-2 text-3xl font-black tracking-widest uppercase">
+            <p>Music</p>
+            <p className="text-lg">Generator</p>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItems />
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <div className="mb-2 flex w-full items-center justify-center gap-1 text-xs">
+          <Credits />
+        </div>
+        <UserButton
+          variant="outline"
+          additionalLinks={[
+            {
+              label: "Customer Portal",
+              href: "/customer-portal",
+              icon: <User />,
+            },
+          ]}
+        />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
